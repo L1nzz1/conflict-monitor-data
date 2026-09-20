8b5183f0c4f04b315c2f0a565c43fd71e1178c78
L1nzz1 <1002954979@qq.com>
2026-09-20 19:02:04 +0800

    data: upgrade agnes model to 3.0-flash

--- update_data.py
+++ update_data.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, 'data.json')
 
 AGNES_BASE = os.environ.get('AGNES_BASE', 'https://apihub.agnes-ai.com/v1')
-AGNES_MODEL = os.environ.get('AGNES_MODEL', 'agnes-2.5-flash')
+AGNES_MODEL = os.environ.get('AGNES_MODEL', 'agnes-3.0-flash')
 AGNES_API_KEY = os.environ.get('AGNES_API_KEY', '')
 
 FEEDS = [
